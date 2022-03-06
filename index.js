0dfec2e0db294c8a0b1fa7b9cc920ce63beae45c
unknown <yakupmalikov@gmail.com>
2022-03-06 14:15:39 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const PORT = process.env.PORT || 8000;
+
+const express = require("express");
+const fs = require("fs");
+const app = express();
+
+var talks = [];
+
+fs.readFile("tedtalk.json", "utf8", (err, data) => {
+  if (err) console.log(err);
+  talks = JSON.parse(data);
+});
+
+app.get("/talks", (req, res) => {
+  res.json(talks);
+});
+
+app.get("/talks/:keyword", (req, res) => {
+  const searchedTalk = req.params.keyword;
+  const specificTalk = talks.filter((talk) =>
+    talk.title.toLowerCase().includes(searchedTalk.toLowerCase())
+  );
+
+  res.json(specificTalk);
+});
+
+app.listen(PORT, () => {
+  console.log(`running on PORT ${PORT}`);
+});
